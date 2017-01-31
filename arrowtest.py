4c7d1d14d9a2ba9f20dd462e69aab0384fea9bd7
Chris Horn <hammerhorn@gmail.com>
2017-01-30 23:21:16 -0500

    slight refactor of 'arrowtest.py'

--- arrowtest.py
+++ arrowtest.py
@@ -6,6 +6,9 @@ detect pressing the arrowkeys
 from cjh.misc import catch_help_flag, notebook
 from versatiledialogs.config import Config
 from versatiledialogs.terminal import Terminal
+
+REMARKS = '    - Move the function to versatiledialogs.terminal.Terminal'
+
 Terminal()
 SHELL = Config().start_user_profile()
 
@@ -13,25 +16,21 @@ def get_arrow_key():
     """
     distinguish between arrow keys being pressed
     """
+    direction = None
     pressed = Terminal.get_keypress()
     if pressed == chr(27):
         pressed = Terminal.get_keypress()
         if pressed == '[':
-            pressed = Terminal.get_keypress()
-            if pressed == 'A':
-                direction = 'up'
-            elif pressed == 'B':
-                direction = 'down'
-            elif pressed == 'C':
-                direction = 'right'
-            elif pressed == 'D':
-                direction = 'left'
-            else:
-                direction = None
-        else:
-            direction = None
-    else:
-        direction = None
+
+            code_dict = {
+                'A': 'up',
+                'B': 'down',
+                'C': 'right',
+                'D': 'left'
+            }
+
+            pressed = Terminal.get_keypress()            
+            direction = code_dict.get(pressed, None)
     return direction
 
 
@@ -43,6 +42,8 @@ def main():
         while True:
             try:
                 Terminal.output('\b' + get_arrow_key())
+
+            # Fails when get_arrow_key() returns None
             except TypeError:
                 pass
     except KeyboardInterrupt:
@@ -50,6 +51,6 @@ def main():
 
 if __name__ == '__main__':
     catch_help_flag(__doc__)
-    notebook('    - Move the function to versatiledialogs.terminal.Terminal')
+    notebook(REMARKS)
     main()
     SHELL.start_app()
