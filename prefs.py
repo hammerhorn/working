56954fd8a937420c7c8957c5ffc46e8b0d68f055
Chris Horn <hammerhorn@gmail.com>
2017-02-01 06:56:44 -0500

    bugs squashed and features added

--- prefs.py
+++ prefs.py
@@ -2,7 +2,7 @@
 """
 Reads from and write to the config file, 'config.json'.
 """
-import argparse
+import argparse, copy, sys
 
 import easycat
 from versatiledialogs.config import Config
@@ -21,90 +21,80 @@ def _parse_args():
     Parse command-line arguments
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-s', type=str, help='set shell')
-    parser.add_argument('-e', type=str, help='set editor')
-    parser.add_argument('-t', type=str, help='set terminal')
-    parser.add_argument('-l', type=str, help='set language')
-    parser.add_argument('-b', type=str, help='set browser')
-
+    parser.add_argument('--shell', '-s', type=str, help='set shell')
+    parser.add_argument('--editor', '-e', type=str, help='set editor')
+    parser.add_argument('--terminal', '-t', type=str, help='set terminal')
+    parser.add_argument('--language', '-l', type=str, help='set language')
+    parser.add_argument('--browser', '-b', type=str, help='set browser')
     parser.add_argument('-v', action='count')
-#    parser.add_argument('-q', action='store_true')
-
     args = parser.parse_args()
     return args
 
-if __name__ == '__main__':
-    ARGS = _parse_args()
-else:
-    ARGS = None
-
+def lookup(key):
+    """return the requested value"""
+    return CONFIG.config_dict.get(key, None)
+    
+ARGS = _parse_args() if __name__ == '__main__' else None
 CONFIG = Config()
 FILENAME = 'versatiledialogs/config.json'
 
-
-def lookup(key):
-    """return the requested value"""
-    if key in list(CONFIG.config_dict.keys()):
-        return CONFIG.config_dict[key]
-    else:
-        return None
-
-SHELL = lookup('shell')
-EDITOR = lookup('editor')
-TERMINAL = lookup('terminal')
-LANGUAGE = lookup('language')
-BROWSER = lookup('browser')
-
+def edit_config_file():  # take off lookup's
+    if ARGS.shell is not None and ARGS.shell not in ['?', lookup('shell')]:
+        CONFIG.write_to_config_file(shell=ARGS.shell)
+    if ARGS.editor is not None and ARGS.editor not in ['?', lookup('editor')]:
+        CONFIG.write_to_config_file(editor=ARGS.editor)
+    if ARGS.terminal is not None and ARGS.terminal not in ['?', lookup('terminal')]:
+        CONFIG.write_to_config_file(terminal=ARGS.terminal)
+    if ARGS.language is not None and ARGS.language not in ['?', lookup('language')]:
+        CONFIG.write_to_config_file(language=ARGS.language.upper())
+    if ARGS.browser is not None and ARGS.browser not in ['?', lookup('browser')]:
+        CONFIG.write_to_config_file(browser=ARGS.browser)
 
 def main():
     """
     Writes requested modifications to the 'config.json' file, and sends
     some kind of feedback to stdout.
     """
-    if ARGS.s is not None:
-        CONFIG.write_to_config_file(shell=ARGS.s)
-    if ARGS.e is not None:
-        CONFIG.write_to_config_file(editor=ARGS.e)
-    if ARGS.t is not None:
-        CONFIG.write_to_config_file(terminal=ARGS.t)
-    if ARGS.l is not None:
-        CONFIG.write_to_config_file(language=ARGS.l.upper())
-    if ARGS.b is not None:
-        CONFIG.write_to_config_file(browser=ARGS.b)
 
     string = ''
 
-    def generate_msg(what_arg, attr_name):
+    def generate_msg(key_name, key_label):
         """
         generate the output to be written to stdout at the appropriate level
         of verbosity
         """
-        if ARGS.v == 2 and what_arg is not None:
-            return_str = "\n{:>25s}: '{}'".format(attr_name, what_arg)
-        elif not (ARGS.v or ARGS.e or ARGS.s or ARGS.t or ARGS.l or ARGS.b):
+        value = lookup(key_name)
+        return "\n{:>25s} = '{}'".format(key_label, value)
 
-            return_str = "\n{:>25s}: '{}'".format(attr_name, SHELL)
-        else:
-            return_str = ''
-        return return_str
+    if ARGS is not None:
+        args_dict = copy.deepcopy(ARGS.__dict__)
+        verbose = 0 if args_dict['v'] is None else args_dict['v']
+        del args_dict['v']
+        noeditcommand = True if args_dict.values() == [None, None, None, None, None] else False
 
+        nochangeneeded = noeditcommand
+        for key in args_dict.keys():
+            if lookup(key) == args_dict[key]:
+                nochangeneeded = True  # needs more efficiency
+        
+    if nochangeneeded is False:
+        edit_config_file()
 
-    string += generate_msg(ARGS.s, 'versatiledialogs mode')
-    string += generate_msg(ARGS.e, 'default text editor')
-    string += generate_msg(ARGS.t, 'default terminal emulator')
-    string += generate_msg(ARGS.l, 'language')
-    string += generate_msg(ARGS.b, 'default web browser')
+    keyname_list = ['shell', 'editor', 'terminal', 'language', 'browser']
+    keylabel_list = ['versatiledialogs mode', 'default text editor', 'default terminal emulator', 'language', 'default web browser']
+        
+    for key_name, key_label in zip(keyname_list, keylabel_list): 
+        if (verbose > 0 and args_dict[key_name] is not None) or noeditcommand is True or args_dict[key_name] == '?':
+            string += generate_msg(key_name, key_label)
 
     if len(string) > 0:
         Terminal.output(string + '\n')
 
-    if (ARGS.v is not None and ARGS.v >= 3) or\
-       (not (ARGS.e or ARGS.s or ARGS.t or ARGS.l or ARGS.b) and
-        ARGS.v is not None and ARGS.v >= 1):
+    if verbose >= 3 or (noeditcommand is True and verbose >= 1):
         Terminal.output('')
         easycat.view_source(FILENAME)
-    if ARGS.v > 0 and (set('setlb') & set(ARGS.__dict__.keys())):
-        print
+    if verbose > 0 and nochangeneeded is False:
+        Terminal.output('')
         Terminal.report_filesave(FILENAME)
         Terminal.clear(2)
         Terminal.report_filesave(FILENAME, fast=True)
