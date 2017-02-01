27a242b7c800c8aeb12f48ac22654a0b6946dd11
Chris Horn <hammerhorn@gmail.com>
2017-01-31 19:50:04 -0500

    added 'fibo.jar' although it probably will not be tweaked much

--- clear_test.py
+++ clear_test.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+"""
+Test of Terminal.clear()
+"""
 import time
 
 import easycat
 from versatiledialogs.terminal import Terminal
+
+DELTA_T = 0.25
+
 Terminal()
 for line in range(Terminal.height()):
     Terminal.output('*' * Terminal.width())
@@ -10,9 +16,9 @@ for line in range(Terminal.height()):
 for n in range(7):
     Terminal.clear(n)
     easycat.write('%d lines cleared\r' % n)
-    time.sleep(.25)
+    time.sleep(DELTA_T)
 
 for n in range(7):
     Terminal.clear(-n)
     easycat.write('%d lines cleared\r' % -n)
-    time.sleep(.25)
+    time.sleep(DELTA_T)
