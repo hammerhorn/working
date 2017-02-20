2987227d76faba79784a8cb631b5972b6a384110
Chris Horn <hammerhorn@gmail.com>
2017-02-20 02:45:32 -0500

    fixing bugs 'prefs.py'

--- prefs.py
+++ prefs.py
@@ -25,7 +25,7 @@ import sys
 import easycat
 from versatiledialogs.config import Config
 from versatiledialogs.terminal import Terminal
-from cjh.misc import notebook  # catch_help_flag
+from cjh.misc import notebook, catch_help_flag
 from ranges import iter_zip
 
 __author__ = 'Chris Horn <hammerhorn@gmail.com>'
@@ -41,46 +41,33 @@ def _parse_args():
     Parse command-line arguments
     """
     notebook(REMARKS)
-#    if '-C' in sys.argv[1:]:
-#        del sys.argv[sys.argv.index('-C')]
-
-    helpflag = True if {'-h', '--help'} & set(sys.argv[1:]) else False
-    if helpflag is True:
-        Terminal.output('')
-    try:
-        parser = argparse.ArgumentParser(
-            description="Edit or view settings for 'versatiledialogs' package." +
-            "  Writes to 'config.json'.  Type 'pydoc prefs' for more info.")
-        parser.add_argument('--shell', '-s', type=str, help='set shell')
-        parser.add_argument('--editor', '-e', type=str, help='set editor')
-        parser.add_argument('--terminal', '-t', type=str, help='set terminal')
-        parser.add_argument('--language', '-l', type=str, help='set language')
-        parser.add_argument('--browser', '-b', type=str, help='set browser')
-        parser.add_argument(
-            '-v', action='count', help="verbose; the more v's, the more vebose")
-        args = parser.parse_args()
-    finally:
-        if helpflag is True:
-            Terminal.output('')
-    return args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shell', '-s', type=str, help='set shell')
+    parser.add_argument('--editor', '-e', type=str, help='set editor')
+    parser.add_argument('--terminal', '-t', type=str, help='set terminal')
+    parser.add_argument('--language', '-l', type=str, help='set language')
+    parser.add_argument('--browser', '-b', type=str, help='set browser')
+    parser.add_argument(
+        '-v', action='count', help="verbose; the more v's, the more verbose")
+    description = """Edit or view settings for 'versatiledialogs' package.  Writes to
+'config.json'.  Type 'pydoc prefs' for more info.\n"""
+    catch_help_flag(help_str=description, argprsr=copy.copy(parser))
+    return parser.parse_args()
 
 
 ARGS = _parse_args() if __name__ == '__main__' else None
 CONFIG = Config()
 FILENAME = 'versatiledialogs/config.json'
-KEYNAME_LIST = ['shell', 'editor', 'terminal', 'language', 'browser']
+KEYNAME_LIST = ('shell', 'editor', 'terminal', 'language', 'browser')
 
-def lookup(key):
-    """return the requested value"""
-    return CONFIG.config_dict.get(key, None)
 
 def edit_config_file():
     """
     Write the changes to the actual file 'config.json'.
     """
     for key_name in KEYNAME_LIST:
-        if ARGS.__dict__[key_name] is not None and\
-           ARGS.shell not in ('?', lookup(key_name)):  # redundent, but safer
+        if ARGS.__dict__[key_name] is not None and\  # redundent, but safer
+           ARGS.shell not in ('?', CONFIG.config_dict.get(key_name, None)):
             CONFIG.config_dict[key_name] = ARGS.__dict__[key_name]
     CONFIG.write_to_config_file(**CONFIG.config_dict)
 
@@ -90,8 +77,6 @@ def main():
     Writes requested modifications to the 'config.json' file, and sends
     some kind of feedback to stdout.
     """
-    outstring = ''
-
     def generate_msg(key_name, key_label):
         """
         generate the output to be written to stdout at the appropriate level
@@ -105,11 +90,12 @@ def main():
         verbose = 0 if args_dict['v'] is None else args_dict['v']
         del args_dict['v']
 
-        nocommand = True if list(args_dict.values()) == [None, None, None, None, None] else False
+        nocommand = tuple(args_dict.values()) == (None, ) * 5
         noeditcommand = True
         for value in args_dict.values():
             if value is not None and value != '?':
                 noeditcommand = False
+                break
 
         supported_modes = (
             'dialog',
@@ -123,7 +109,8 @@ def main():
 
         if args_dict['shell'] not in (supported_modes + ('?', None)):
             args_dict['shell'] = CONFIG.config_dict['shell']
-            Terminal.output(''.join(('\nsupported modes: ', str(supported_modes)[1:-1], '\n')))
+            Terminal.output(
+                '\nsupported modes: {}\n'.format(str(supported_modes)[1:-1]))
 
         nochangeneeded = noeditcommand
         for key in args_dict.keys():
@@ -141,7 +128,7 @@ def main():
         'default web browser'
     )
 
-    msg_list = [outstring]
+    msg_list = []
     for key_name, key_label in iter_zip(KEYNAME_LIST, keylabel_list):
         if (verbose > 0 and args_dict[key_name] is not None) or\
             nocommand is True or args_dict[key_name] == '?':
